aed31295ba77d333902353905adbc193ea2b2d90
Maha Mahmood <>
2020-12-29 13:24:31 -0500

    created and styled break ice button. added it to home. added some style to home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import BreakIceButton from '../BreakIceButton/BreakIceButton.js';
 
 export default function Home() {
     return (
-        <div>
+        <div className='home'>
             home
+            <br></br>
+            <BreakIceButton />
         </div>
     )
 }

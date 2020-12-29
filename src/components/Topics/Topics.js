e888bc3d84cec78e7e89211c52b66a2b876d3499
Maha Mahmood <>
2020-12-29 17:04:21 -0500

    created and styled topic categories

--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import TopicCategories from './TopicCategories.js';
+import BreakIceButton from '../BreakIceButton/BreakIceButton';
+import BottomNav from '../Nav/BottomNav';
+import './Topics.css';
 
 export default function Topics() {
     return (
-        <div>
-            topics page
-        </div>
+        <>
+            <div className='Topics'>
+                <h1>Discussion Topics</h1>
+                <p>Choose some topics and break the ice!</p>
+                <TopicCategories />
+                <BreakIceButton />
+            </div>
+            <BottomNav />
+        </>
     )
 }

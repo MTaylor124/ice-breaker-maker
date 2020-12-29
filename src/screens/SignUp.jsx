3be12a246cbb0bac5d24fa86e3db54341804aa6d
MTaylor124 <matthew_taylor1@student.uml.edu>
2020-12-29 15:09:52 -0500

    added firebase auth session persistence, sign in and login sets auth status to true

--- src/screens/SignUp.jsx
+++ src/screens/SignUp.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './SignUp.css'
 import Button from '@material-ui/core/Button'
 import { useHistory } from "react-router-dom"
 import firebase from 'firebase/app'
 import 'firebase/auth'
 
+import { GlobalContext } from '../components/context/GlobalContext';
+
 function SignUp(props) {
   const history = useHistory()
+  let {
+    auth
+}= useContext(GlobalContext)
   return (
     <div className="signup-container">
       <h1 className="app-name">Ice Breaker Maker</h1>
@@ -17,9 +22,8 @@ function SignUp(props) {
             e.preventDefault()
             firebase.auth().createUserWithEmailAndPassword(e.target[0].value, e.target[1].value)
             .then(() => {
-                console.log('signed up!')
-                // redirect to homepage/dashboard
-                history.push('/home')
+                auth.signIn()
+                history.push('/')
             })
             .catch(err => {
                 console.error(err.code)

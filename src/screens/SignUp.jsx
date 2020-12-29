45a1fe5aa1436e6f9331c5034d76b2003ac40529
MTaylor124 <matthew_taylor1@student.uml.edu>
2020-12-29 11:27:21 -0500

    added sign up logic

--- src/screens/SignUp.jsx
+++ src/screens/SignUp.jsx
@@ -2,12 +2,28 @@ import React from 'react';
 import './SignUp.css'
 import Button from '@material-ui/core/Button'
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
 function SignUp(props) {
   return (
     <div className="signup-container">
       <h1 className="app-name">Ice Breaker Maker</h1>
       <h3>Create an Account</h3>
-      <form className="signup-form">
+      <form 
+        className="signup-form"
+        onSubmit={(e) => {
+            e.preventDefault()
+            firebase.auth().createUserWithEmailAndPassword(e.target[0].value, e.target[1].value)
+            .then(() => {
+                console.log('signed up!')
+                // redirect to homepage/dashboard
+            })
+            .catch(err => {
+                console.error(err.code)
+                // show error with sign up
+            })
+        }}>
       <label>Email:
        <input
           placeholder="Email"
@@ -23,7 +39,14 @@ function SignUp(props) {
           />
         </label>
         <br />
-        <Button className="submit-button" variant="contained" color="primary">Submit</Button>
+        <Button 
+            className="submit-button" 
+            variant="contained" 
+            color="primary" 
+            type="submit"
+            >
+            Submit
+        </Button>
         </label>
         </form>
     </div>

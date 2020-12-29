b3b9a134fff4bdbe21f773adf1720ae3c0dba09e
Dean <dean.g.leong@gmail.com>
2020-12-29 17:08:30 -0500

    card page

--- src/components/IceBreakers/IceBreakersSingle.js
+++ src/components/IceBreakers/IceBreakersSingle.js
@@ -7,8 +7,9 @@ export default function IceBreakersSingle() {
       <div className="icebreakersingle-containter">
         <div className="icebreaker-card">
           <h2 className="card-title">Question</h2>
-          <hr className="card-hr"/>
-           <p className="icebreaker-text">What is your favorite movie?</p>
+            <hr className="card-hr"/>
+              <p className="icebreaker-text">If you had lived during the Gold Rush, would you have journied west to strike it rich?</p>
+          {/* above is where a js function will go to display an icebreaker */}
         </div>
         <button className="BreakIceBtn-2">BREAK THE ICE</button>
       </div>

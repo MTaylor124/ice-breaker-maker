f91e033a9a0fe4583eeabc243225182e62aa0323
MTaylor124 <matthew_taylor1@student.uml.edu>
2020-12-29 14:44:00 -0500

    commented out button

--- src/components/BreakIceButton/BreakIceButton.js
+++ src/components/BreakIceButton/BreakIceButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './BreakIceButton.css';
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
 
 function BreakIceButton(props) {
     return(

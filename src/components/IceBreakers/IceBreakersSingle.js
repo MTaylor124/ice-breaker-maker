49c452e8002a34d45b97de1bc2b963ebb5ad5b5c
Dean <dean.g.leong@gmail.com>
2020-12-29 16:01:18 -0500

    break ice button

--- src/components/IceBreakers/IceBreakersSingle.js
+++ src/components/IceBreakers/IceBreakersSingle.js
@@ -4,9 +4,13 @@ import './IceBreakersSingle.css'
 export default function IceBreakersSingle() {
     return (
       <div className="icebreakersingle-containter">
-          <div className="icebreaker-card">
+        <div className="icebreaker-card">
+          <h2 className="card-title">Question</h2>
+          <hr className="card-hr"/>
            <p className="icebreaker-text">What is your favorite movie?</p>
           </div>
         </div>
     )
 }
+
+//import the break ice button?

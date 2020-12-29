2b7fe5ebb56212c1a09ac079cc07941d7c8c7e48
Dean <dean.g.leong@gmail.com>
2020-12-29 10:07:17 -0500

    added buttons for login, starting google auth

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Login.css'
+import Button from '@material-ui/core/Button'
+// import firebase from 'firebase'
 
 function Login(props) {
+
+  // let provider = new firebase.auth.GoogleAuthProvider();
+
   return (
-    <div>
+    <div className="login-container">
       <h1 className="app-name">Ice Breaker Maker</h1>
+        <div className="login-buttons-container">
+          <Button className="login-button" variant="contained" color="primary">Login With Google</Button>
+          <Button className="login-button" variant="contained" color="primary">Login</Button>
+        </div>
     </div>
   );
 }

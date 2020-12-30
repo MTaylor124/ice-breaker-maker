71c23a521017a2dd1080f2455ee2b8eea80e542c
Maha Mahmood <>
2020-12-30 10:57:59 -0500

    created and styled activities page. edited topics css

--- src/components/Activities/ActivitiesAll.js
+++ src/components/Activities/ActivitiesAll.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import BreakIceButton from '../BreakIceButton/BreakIceButton';
+import BottomNav from '../Nav/BottomNav';
+import './ActivitiesAll.css';
+import ActivityCategories from './ActivityCategories';
 
 export default function Activities() {
     return (
-        <div>
-            ALL activities or FILTERED activities
-        </div>
-    )
+        <>
+            <div className='ActivitiesAll'>
+                <h1>Activities</h1>
+                <p>Choose some activities and break the ice!</p>
+                <ActivityCategories />
+                <BreakIceButton />
+            </div>
+            <BottomNav />
+        </>
+    );
 }
